cbe47fcc7f7a39c896470ad4f919142808f47248
andrewoseen <amo.vfx@protonmail.com>
2021-07-02 13:47:49 +0200

    Calling functions from python

--- setcontext/setcontext.sh
+++ setcontext/setcontext.sh
@@ -1,11 +1,21 @@
-alias validate_gcloud_py='/Users/andrewoseen/git/amo-prj-utilities/validate_project_name/main.py'
-alias create_dir_py='/Users/andrewoseen/git/amo-prj-utilities/setcontext/main.py'
+alias validate_gcloud_py='/Users/andrewoseen/git/amo-prj-utilities/validate_project_name/setcontext.py'
+alias create_dir_py='/Users/andrewoseen/git/amo-prj-utilities/setcontext/setcontext.py'
 
+alias pprint="python /Users/andrewoseen/git/amo-prj-utilities/setcontext/setcontext.py pprint"
+
+
+set_project_envvar () {
+  pprint "Setting environment variables" green 1
+  eval `python /Users/andrewoseen/git/amo-prj-utilities/setcontext/setcontext.py set_env $1`
+}
 
 validate_gcloud_project () {
 
 	tput setaf 3; echo Validating gcloud project: $context | sed 's/^/  /'
 	local valid_project=$(python /Users/andrewoseen/git/amo-prj-utilities/setcontext/validate_gcloud_project.py $1)
+	#if project exists, set gcloud
+	#if project is invalid exit
+	#if project doesn't exist and is valid create and set
 	if [ $valid_project -eq 0 ]
 	then
 		tput setaf 2; echo $context is a valid project | sed 's/^/    /'
@@ -15,10 +25,15 @@ validate_gcloud_project () {
 	fi
 }
 
+set_gcloud_project () {
+  tput setaf 3; echo setting gcloud project: $1 | sed 's/^/  /'
+  gcloud config set project $1
+}
+
 change_directory () {
 	
 	tput setaf 3;echo Creating directories... | sed 's/^/  /'
-	local output_dir=$(python /Users/andrewoseen/git/amo-prj-utilities/setcontext/main.py $1)
+	local output_dir=$(python /Users/andrewoseen/git/amo-prj-utilities/setcontext/setcontext.py $1)
 	cd $output_dir
 }
 
@@ -27,7 +42,7 @@ create_conda_env () {
 	conda create -y -q --name $1 python=3.8
 	conda activate $1
 	#pip install requirements for a flask project
-	pip install flask
+	pip install -y flask
 }
 
 initialize_git_repo () {
@@ -42,17 +57,31 @@ newcontext () {
 	
 	validate_gcloud_project $context
 	change_directory $context
-	create_conda_env $context
-	initialize_git_repo
+	#create_conda_env $context
+	#initialize_git_repo
 }
 
-setcontext () {
-	local context=$1
+set_context () {
+	#split text and store in env, validate values
+	#set gcloud project
+	#build/change directories
+	#set change conda env
+	#init git
 
-	tput setaf 4; echo Setting context: $1
-	change_directory $context
-	conda activate $context
 
+
+	pprint "Setting context to: $1" green
+	set_project_envvar $1
+  #set_gcloud_project $context
+	#change_directory $context
+	#conda activate $context
+
+}
+
+delcontext () {
+  #this is meant to destr
+  local context=$1
+	tput setaf 1; echo Deleting context: $context
 }
 
 
