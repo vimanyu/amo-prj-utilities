dd4ae74ab4fb9159b0a47fef185892f439a2f77d
andrewoseen <amo.vfx@protonmail.com>
2021-07-04 16:13:54 +0200

    seperated out functions split_namespace now handles spliting the name space into a tuple set_environment_variable now takes a type and a value. Added class CONTEXT to avoid testing strings of contexts. Added context existing functions. Added is_version_string_valid to check if string matches vXXX Replaced valid_envar with is_environment_variable_valid Added create_conda_env Added set_conda_env

--- setcontext/setcontext.py
+++ setcontext/setcontext.py
@@ -17,18 +17,25 @@ from termcolor import cprint
 function to handle bad version string
 """
 
+PATH = pathlib.Path.home() / "git"
 
-def valid_envvar(var: str) -> bool:
+class CONTEXT:
+    PROJECT = "PROJECT"
+    SERVICE = "SERVICE"
+    VERSION = "VERSION"
+
+
+def is_environment_variable_valid(var: str) -> bool:
     return (var in os.environ.keys()) and (os.environ[var] != "")
 
-def validate_project_name(self, name: str) -> bool:
+def is_project_name_valid(name: str) -> bool:
     patterns = '^[a-z0-9/-]*$'
     if re.search(patterns, name):
         return True
     else:
         return False
 
-def validate_gcloud_projects(self, project_name: str) -> bool:
+def does_gcloud_project_exist(project_name: str) -> bool:
     output = subprocess.check_output("gcloud projects list --format json".split(" "))
     json_str = output.decode('utf-8')
     json_data = json.loads(json_str)
@@ -36,24 +43,75 @@ def validate_gcloud_projects(self, project_name: str) -> bool:
 
     return False if project_name in project_names else True
 
+def does_project_exist(project_name: str) -> bool:
+    path = PATH
+    path /= project_name
+    return path.is_dir()
+
+def does_service_exist(service_or_version_name: str) -> bool:
+    path = pathlib.Path.cwd()
+    path /= service_or_version_name
+    return path.is_dir()
+
+def is_version_string_valid(version_string: str):
+    #replace with regex
+    return version_string.startswith("v") and version_string[1:].isdigit() and len(version_string) == 4
+
+def split_namespace(namespace: str) -> tuple(str):
+    project = ""
+    service = ""
+    version = ""
+
+    # replace with argparse for better error handling and help?
+    if not ":" in namespace:
+        project = namespace
+        service, version = ""
+
+    else:
+        split_context = namespace.split(":")
+        split_count = len(split_context)
+
+        if split_count == 2:
+            project, service = split_context
+            version = ''
+
+        if split_count == 3:
+            project, service, version = split_context
+            is_version_string_valid(version)
+
+    return project, service, version
+
+def set_environment_variable(type: str, value: str):
+    """
+
+    :param type:
+        The type of variable replaced to set context, either PROJECT, SERVICE or VERSION
+    :param value:
+    """
+    type_upper = type.upper()
+    if type_upper in ["PROJECT", "SERVICE", "VERSION"]:
+        print(f"echo Setting {type_upper} to {value};")
+        print(f"export {type_upper}={value};")
+
+
 class SetContext(object):
 
-    def build_directory_path(self):
+    def change_directory_path(self, project=None, service=None, version=None):
         """
         This builds the path of the project from environment variables. If no project environment variables exist,
         Then the directory defaults to ~/git
 
         eval `python setcontext.py build_directory_path`
         """
-        path = pathlib.Path.home() / "git"
-        if valid_envvar("PROJECT"):
-            path /= os.environ['PROJECT']
+        path = PATH
+        if project:
+            path /= project
 
-        if valid_envvar("SERVICE"):
-            path /= os.environ['SERVICE']
+        if service:
+            path /= service
 
-        if valid_envvar("VERSION"):
-            path /= os.environ['VERSION']
+        if version:
+            path /= version
 
         # add beta or alpha flags
 
@@ -68,32 +126,37 @@ class SetContext(object):
     #memoize
 
 
+    def set_environment_variables(self, context_string=None):
+        """
+        Prints a string for a bash shell to evaulate.
 
-    """This is really gross but I dont really have a choice."""
-    """eval `python setcontext.py set_env "myprj:myserv:v001"`"""
-    def set_env(self, project_name=None):
+        eval `python setcontext.py set_environment_variables myproject:myservice:v001`
 
+        :param context_string:
+            myproject:myservice:v001
+        """
         project = ""
         service = ""
         version = ""
 
         # replace with argparse for better error handling and help?
-        if not ":" in project_name:
-            project = project_name
-            version = ""
+        if not ":" in context_string:
+            project = context_string
+            service, version = ""
 
         else:
-            split_context = project_name.split(":")
+            split_context = context_string.split(":")
             split_count = len(split_context)
 
             if len(split_context) == 2:
                 project, service = split_context
+                version = ''
 
             if len(split_context) == 3:
                 project, service, version = split_context
-                validate_version_str(version)
+                is_version_string_valid(version)
 
-        if self.validate_project_name(project):
+        if is_project_name_valid(project):
             print(f"echo Setting PROJECT to {project};")
             print(f"export PROJECT={project};")
 
@@ -107,11 +170,11 @@ class SetContext(object):
     """eval `python setcontext.py set_terminal_prompt`"""
     def set_terminal_prompt(self):
         prompt_string = "PS1="
-        if valid_envvar("PROJECT"):
+        if is_environment_variable_valid("PROJECT"):
             prompt_string += "'%F{55}'${PROJECT}"
-        if valid_envvar("SERVICE"):
+        if is_environment_variable_valid("SERVICE"):
             prompt_string += "'%F{default}:%F{46}'${SERVICE}"
-        if valid_envvar("VERSION"):
+        if is_environment_variable_valid("VERSION"):
             prompt_string += "'%F{default}:%F{38}'${VERSION}"
 
 
@@ -119,8 +182,16 @@ class SetContext(object):
         print(prompt_string)
 
 
+
+
     def create_project(self):
         pass
+
+    def create_conda_env(self, env_name):
+        print(f"conda create -y -q --name {env_name} python=3.9")
+
+    def set_conda_env(self, env_name):
+        print(f"conda activate {env_name}")
 
     def set_project(self):
         print("conda activate ${PROJECT}")
@@ -136,10 +207,7 @@ class SetContext(object):
 
 
 
-
-
-
-    def setcontext(self, context: str):
+    def setcontext(self, namespace: str):
         """
         Main program to call all the sub functions for setting context between projects.
         This generates a bash script to be called from the shell and evaluated.
@@ -147,27 +215,50 @@ class SetContext(object):
         Ex:
         eval `python setcontext.py setcontext my_project_name:my_service_or_module/v<001-999>
 
-        :param context:
+        :param namespace:
             A string to determine where you are working. project:service:version
         """
-        if not valid_envvar("PROJECT"):
+
+        project, service, version = split_namespace(namespace)
+        if project and is_project_name_valid(project):
+            if does_project_exist():
+
+
+            else:
+                if is_project_name_valid(project):
+                    if not does_gcloud_project_exist():
+
+
+
+            is_project_name_valid(namespace)
+
+
+
+        if not is_environment_variable_valid("PROJECT"):
+            if os.environ["PROJECT"] != namespace:
+
+
             #create project
-            self.validate_project_name(context)
-            self.validate_gcloud_projects()
-            self.set_env()
+
+            self.
+            self.set_environment_variables()
+            self.create_conda_env(os.environ['PROJECT'])
+            self.set_conda_env(os.environ['PROJECT'])
             self.set_terminal_prompt()
         else:
             #set path to change dir
+            self.change_directory_path(project=os.environ["PROJECT"])
             #set conda env
             #set gcloud project
             #set terminal prompt
             #serach for service
-            if not valid_envvar("SERVICE"):
+            if not is_environment_variable_valid("SERVICE"):
                 #create service
                 pass
 
             else:
                 pass
+                self.build_directory_path(project=os.environ["PROJECT"])
                 #change to service
 
 
@@ -177,14 +268,7 @@ class SetContext(object):
         cprint("\t"*indent + msg, color)
 
 
-def validate_version_str(version_string: str):
-    #replace with regex
-    if not version_string.startswith("v"):
-        sys.exit(f"version string: {version_string} doesn't start with v")
-    if not version_string[1:].isdigit():
-        sys.exit(f"version string: {version_string} doesnt end in numerics 00 - 99")
-    if not len(version_string) == 4:
-        sys.exit(f"version string: {version_string} doesnt end in numerics 000 - 999")
+
 
 
 
